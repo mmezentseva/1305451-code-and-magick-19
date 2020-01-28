07b93ef67700a1607c8bdf3b66c2669449caf84e
Marina Mezentseva <marinamezentseva07@gmail.com>
2020-01-29 01:37:58 +0300

    bar_statistics_errow_correction

--- js/stat.js
+++ js/stat.js
@@ -7,8 +7,15 @@ var CLOUD_Y = 10;
 var CLOUD_GAP = 10;
 var GAP = 20;
 var FONT_GAP = 16;
+var FONT_STYLE = '16px PT Mono';
+var FONT_COLOR = '#000';
+var FONT_TEXT = 'hanging';
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
+var YOU = 'Вы';
+var YOU_BAR_COLOR = 'rgba(255, 0, 0, 1)';
+var barHueColor = 240;
+var barLightnessColor = 50;
 var barHeight = 150;
 var barTopPoint = 180;
 
@@ -20,7 +27,7 @@ var renderCloud = function (ctx, x, y, color) {
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
-  for (var i = 0; i < arr.length; i++) {
+  for (var i = 1; i < arr.length; i++) {
     if (arr[i] > maxElement) {
       maxElement = arr[i];
     }
@@ -32,9 +39,9 @@ window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.textBaseline = 'hanging';
+  ctx.fillStyle = FONT_COLOR;
+  ctx.font = FONT_STYLE;
+  ctx.textBaseline = FONT_TEXT;
   ctx.fillText('Ура вы победили!', CLOUD_X + GAP, CLOUD_Y + GAP);
   ctx.fillText('Список результатов:', CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP);
 
@@ -43,14 +50,17 @@ window.renderStatistics = function (ctx, names, times) {
   for (var i = 0; i < names.length; i++) {
     var currentHeight = barHeight * times[i] / maxTime;
 
-    ctx.fillStyle = '#000';
+    ctx.fillStyle = FONT_COLOR;
     ctx.fillText(names[i], CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - GAP);
     ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - barTopPoint - GAP + (barHeight - currentHeight), BAR_WIDTH, currentHeight);
 
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    if (names[i] === YOU) {
+      ctx.fillStyle = YOU_BAR_COLOR;
     } else {
-      ctx.fillStyle = 'hsl(' + 240 + ',' + Math.round(Math.random() * 100) + '%' + ',' + Math.round(Math.random() * 50) + '%' + ')';
+      var getRandomNumber = function (hue, lightness) {
+        return 'hsl(' + hue + ',' + Math.round(Math.random() * 100) + '%' + ',' + lightness + '%' + ')';
+      };
+      ctx.fillStyle = getRandomNumber(barHueColor, barLightnessColor);
     }
 
     ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - barTopPoint + (barHeight - currentHeight), BAR_WIDTH, currentHeight);
